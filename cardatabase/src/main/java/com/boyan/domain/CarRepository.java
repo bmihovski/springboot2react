98d4d734cb9fde99a7fc07a4065eeb30c3cdbcb2
Gogs <gogs@fake.local>
2019-08-05 19:15:35 +0300

    Restfull service added

--- cardatabase/src/main/java/com/boyan/domain/CarRepository.java
+++ cardatabase/src/main/java/com/boyan/domain/CarRepository.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long>{
 	//Fetch cars by color
-	List<Car> findByColor(String color);
+	List<Car> findByColor(@Param("color") String color);
 	//Fetch cars by year
 	List<Car> findByYear(int year);
 	//Fetch cars by brand and model
@@ -18,7 +21,7 @@ public interface CarRepository extends CrudRepository<Car, Long>{
 	List<Car> findByBrandOrderByYearAsc(String brand);
 	//Fetch cars by brand using SQL
 	@Query("select c from Car c where c.brand = ?1")
-	List<Car> findByBrand(String brand);
+	List<Car> findByBrand(@Param("brand") String brand);
 	//Fetch cars by brand using SQL
 	@Query("select c from Car c where c.brand like %?1")
 	List<Car> findByBrandEndsWith(String brand);
